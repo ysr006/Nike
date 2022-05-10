89a30d730d3884fa855fda16b2f088b06af6fcf0
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-05-10 11:54:08 +0000

    Update partition.py

--- lib/partition.py
+++ lib/partition.py
@@ -1,16 +1,17 @@
 import os
 
-girdi1 = ""
-girdi3 = ""
-
 class Partition():
+    gridi1 = ""
     kod1 = 'echo "n\n\n\n\n\nw\n" | gdisk /dev{}'.format(girdi1)
     os.system(kod1)
 
 class Uefi():
+    girdi = ""
     kod2 = 'echo "n\n\n\n+512M\nef00\nw\n" | gdisk /dev{}'.format(girdi1)
     os.system(kod2)
 
 class Swap():
+    girdi1 = ""
+    girdi3 = ""
     kod3 = 'echo "n\n\n\n+{}\n8200\nw\n" | gdisk /dev{}'.format(girdi1,girdi3)
     os.system(kod3)
