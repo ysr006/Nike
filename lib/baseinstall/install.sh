be87efa54497506262922eaedd6967fe9ff3f1c0
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-05-15 04:38:53 +0000

    Add files via upload

---
+++ lib/baseinstall/install.sh
@@ -0,0 +1,2 @@
+genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux-firmware networkmanager nano sh sudo
---
+++ lib/mount/btrfs.sh
@@ -0,0 +1,4 @@
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+umount -R /mnt
